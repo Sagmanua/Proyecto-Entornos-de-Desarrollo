c3e158110c35b0d7edda44ee660d054139a5837e
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-12-26 16:49:15 +0100

    v0.1 crete funcion profile

---
+++ versiones/v0.1 another structure/actions/User_name.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/connect_bd.php';
+
+$sql = "SELECT username FROM users WHERE id = " . $_SESSION['user_id'];
+$result = $conn->query($sql);
+
+if ($result && $result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    echo $row['username'];
+}
+
+
+?>
---
+++ versiones/v0.1 another structure/index.php
@@ -0,0 +1,6 @@
+<?php include "actions/head.php"; ?>
+
+<p>This is index front page </p>
+
+
+<?php include "actions/foot.php"; ?>

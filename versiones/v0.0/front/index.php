24ddebe9885f57f7269cb32fdddb217919ebfd8a
Sagmanua <163444640+Sagmanua@users.noreply.github.com>
2026-01-07 11:46:54 +0100

    1

--- versiones/v0.0/front/index.php
+++
@@ -1,5 +0,0 @@
-<?php include "inc/head.php"; ?>
-<p>This is index front page </p>
-
-
-<?php include "inc/foot.php"; ?>
--- versiones/v0.2 create write mysql databases/login.php
+++
@@ -1,8 +0,0 @@
-<?php require_once __DIR__ . '/inc/login_user.php'; ?>  
-<form method="POST">
-    <h2>login_system</h2>
-    <input type="text" name="username" required placeholder="Username"><br><br>
-    <input type="password" name="password" required placeholder="Password"><br>
-    <small>Min. 8 characters + 1 number</small><br><br>
-    <button name="login">Login</button>
-</form>
